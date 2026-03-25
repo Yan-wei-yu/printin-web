3e3f108380c4f7333f009de6bd50c4cb12f84184
Yan <asd25845619@gmail.com>
2026-03-25 22:34:25 +0800

    fix: remove implicit any types in ProductDetail

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,15 +1,7 @@
-// pages/ProductDetail.tsx
-import Navbar from "../components/layout/Navbar";
-import Footer from "../components/layout/Footer";
-
-export default function ProductDetail({ lang, setLang, t }) {
+export default function ProductDetail() {
   return (
-    <>
-      <Navbar lang={lang} setLang={setLang} t={t} />
-      <main>
-        {/* 這頁的內容 */}
-      </main>
-      <Footer t={t} />
-    </>
+    <main>
+      {/* Product detail page */}
+    </main>
   );
 }
